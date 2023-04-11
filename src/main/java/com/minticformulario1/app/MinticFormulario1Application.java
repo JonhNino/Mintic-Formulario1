ce90b68f05fa2029408d98924bef3ba576329925
JonhNino <jonhninoa@gmail.com>
2023-04-10 22:14:13 -0500

    Mintic-Formulario1

---
+++ src/main/java/com/minticformulario1/app/MinticFormulario1Application.java
@@ -0,0 +1,13 @@
+package com.minticformulario1.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinticFormulario1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinticFormulario1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/minticformulario1/app/MinticFormulario1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minticformulario1.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinticFormulario1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
